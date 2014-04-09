a0841bf8d347387fe699778ae2241d6f8ea63054
James Harris <james.harris@icecave.com.au>
2014-04-10 09:10:47 +1000

    Added min/max length methods to interface.

--- src/PasswordGeneratorInterface.php
+++ src/PasswordGeneratorInterface.php
@@ -1,6 +1,9 @@
 <?php
 namespace Icecave\Abraxas;
 
+/**
+ * Interface for password generators.
+ */
 interface PasswordGeneratorInterface
 {
     /**
@@ -9,4 +12,32 @@ interface PasswordGeneratorInterface
      * @return string The generated password.
      */
     public function generate();
+
+    /**
+     * Fetch the minimum length of generated passwords.
+     *
+     * @return integer The minimum length of generated passwords.
+     */
+    public function minimumLength();
+
+    /**
+     * Set the minimum length of generated passwords.
+     *
+     * @param integer $length The minimum length of generated passwords.
+     */
+    public function setMinimumLength($length);
+
+    /**
+     * Fetch the maximum length of generated passwords.
+     *
+     * @return integer The maximum length of generated passwords.
+     */
+    public function maximumLength();
+
+    /**
+     * Set the maximum length of generated passwords.
+     *
+     * @param integer $length The maximum length of generated passwords.
+     */
+    public function setMaximumLength($length);
 }
